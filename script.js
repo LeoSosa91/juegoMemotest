88db3b28486d9efb8775abbdc4b56077583bfb5a
Pablo Romano <patoromano22@gmail.com>
2022-10-16 20:31:30 -0300

    informacion modal
    se agrego informacion al modal que se muestra, en base a la seleccion acertada

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 var iconos = []
 var selecciones = []
-
+var descripciones = []
+var titulos = []
 generarTablero()
 
 /* function cargarIconos() {
@@ -43,6 +44,67 @@ generarTablero()
 } */
 
 function cargarIconos() {
+    moluscosTitulo1 = [
+        'Caracol Pie Violeta',
+        'Caracol Blanco',
+        'Pulpo Colorado',
+        'Centolla',
+        'Langostino',
+    ]
+
+    moluscosTitulo2 = [
+        'Almeja',
+        'Sombrerito',
+        'Mejillón',
+        'Cholga',
+        'Caracol Pie rojo',
+    ]
+
+    pecesTitulo1 = [
+        'Salmón de Mar',
+        'Undaria / Wakame',
+        'Lechuga de Mar',
+        'Escrófalo',
+        'Piropia / Luche',
+    ]
+
+    pecesTitulo2 = [
+        'Merluza',
+        'Pejerrey',
+        'Pez Elefante',
+        'Pez Sapo',
+        'Róbalo',
+    ]
+
+    moluscosTexto1 = [
+        'Puede medir hasta 20 cm de largo y su talla mínima de captura son 11 cm.',
+        'Especie de caracol que habita el intermareal y submareal desde las costas de Bs As hasta Tierra del Fuego. Son carnívoros, se alimentan de mejillones, almejas, caracoles y bivalvos produciendo un agujero en sus valvas para acceder a la carne de los mismos.',
+        'Es un organismo de la clase gasterópoda junto con calamares y calamaretes. Se lo encuentra entre la franja del Golfo San Matías y el Canal de Beagle. Presenta sexos separados y la hembra pone huevos una sola vez en su vida.',
+        'Se aparean en diciembre y se puede diferenciar machos y hembras por la forma de su abdomen. Son animales que crecen por mudas y se estima que podría vivir hasta 20 años. Comen de todo, son carroñeras principalmente.',
+        'Junto con la merluza, es uno de los principales recursos económicos del país. Es posible diferenciar machos de hembras por su primer par de patas. Predador importante de organismos bentónicos como crustáceos, gusanos y moluscos.',
+    ]
+    moluscosTexto2 = [
+        'Molusco bivalvo que se encuentra en fondos blandos de hasta 60 m de profundidad.Pueden vivir hasta 34 años y llegar a medir 55 cm.',
+        'Molusco gasterópodo que habita en el intermareal rocoso somero. Son raspadores herbívoros generalistas. Para su captura es necesario que al menos midan 2 cm.',
+        'Molusco Bivalvo que habita en la mayoría de las costas de Sudamérica y se lo encuentra hasta los 50 metros de profundidad. Es una especie afectada por la marea roja por ser su hábito alimentario, la filtración. En su estadio juvenil se fija al fondo mediante unos filamentos bísales que constituyen el biso.',
+        'Molusco bivalvo que se diferencia del mejillón por presentar líneas longitudinales llamadas costillas. Son organismos filtradores y su dieta es principalmente fitoplancton por lo que se ve afectado por la marea roja.',
+        'Es uno de los caracoles más grandes del golfo y depreda bivalvos y otros caracoles. Para recolectarlo es necesario bucear desde costa o embarcado.',
+    ]
+    pecesTexto1 = [
+        'Es el pez más grande de los arrecifes patagónicos. Depredador generalista (peces, pulpos, cangrejos, etc.). Se Distribuye desde Río de Janeiro hasta el Golfo San Jorge.',
+        'Alga parda originaria de Japón, sur de China y Corea. Se cree que entró en Argentina a inicios de los 90s. Se la utiliza tanto para alimentación humana como de animales, como fertilizante o para la industria farmacéutica.',
+        'Alga verde formada por dos capas de células, La especie tiene una amplia distribución en todo el mundo, se encuentran tanto en la zona de la costa como en profundidad. Usos: Consumo humano directo, aporta cantidades significativas de minerales y vitaminas A y C. En la elaboración de cosméticos hidratantes. Como Fertilizante agrícola.',
+        'Depredador que se alimenta de crustáceos y otros peces. Es el pez más abundante de los arrecifes del Golfo San Jorge.',
+        'Alga roja que puede alcanzar un máximo de 40 cm de diámetro, de color pardo dorado hasta púrpura, a veces iridiscentes. En Argentina se la puede hallar desde Río Negro hasta Tierra del Fuego, pero las poblaciones más abundantes y permanentes se encuentran en el golfo San Jorge y costas de Santa Cruz. Consumo humano directo. La recolección comercial o para consumo propio del “luche” se realiza manualmente durante la bajamar.',
+    ]
+    pecesTexto2 = [
+        'Uno de los principales recursos del mar argentino. Los machos pueden vivir hasta 9 años y las hembras hasta 11 años. Son animales carnívoros. ',
+        'Peces costeros que habitan hasta los 10-50 metros de profundidad dependiendo de la especie. Los peces grandes se alimentan de los más pequeños.',
+        'Pariente de tiburones y rayas. Se distribuye desde el sur de Brasil hasta el sur de Perú por el Pacífico.',
+        'Pez escurridizo que habita en cuevas y se mimetiza con los fondos. Se reproduce entre el invierno y la primavera.',
+        'Se distribuye desde Valparaíso, Chile hasta el sur de la provincia de Bs As incluidas las islas Malvinas. Es una especie Protándrica, son machos primero y luego de una determinada edad se convierten en hembras. Forma pequeños cardúmenes en aguas poco profundas y fondos arenosos.',
+    ]
+
     moluscosCrustaceos1 = [
         '<img src="assent/images/caracol2.jpeg" class="img-fluid d-block mx-auto my-auto imagen"  alt="...">',
         '<img src="assent/images/caracol3.jpeg" class="img-fluid d-block mx-auto my-auto imagen"  alt="...">',
@@ -76,17 +138,25 @@ function cargarIconos() {
     if (random > 0) {
         if (Math.random() - 0.5 > 0) {
             iconos = moluscosCrustaceos1
+            descripciones = moluscosTexto1
+            titulos = moluscosTitulo1
             categoria = "Moluscos y Crustáceos"
         } else {
-            iconos = moluscosCrustaceos1
+            iconos = moluscosCrustaceos2
+            descripciones = moluscosTexto2
+            titulos = moluscosTitulo2
             categoria = "Moluscos y Crustáceos"
         }
     } else {
         if (Math.random() - 0.5 > 0) {
             iconos = pecesAlgas1
+            descripciones = pecesTexto1
+            titulos = pecesTitulo1
             categoria = "Peces y Algas"
         } else {
             iconos = pecesAlgas2
+            descripciones = pecesTexto2
+            titulos = pecesTitulo2
             categoria = "Peces y Algas"
         }
 
@@ -103,6 +173,8 @@ function generarTablero() {
     selecciones = []
     let tablero = document.getElementById("tablero")
     let tarjetas = []
+    let textos = []
+    let headers = []
     for (let i = 0; i < 10; i++) {
         tarjetas.push(`
                 <div class="area-tarjeta" id="area-tarjeta${i}" onclick="seleccionarTarjeta(${i})">
@@ -111,19 +183,25 @@ function generarTablero() {
                             ${iconos[0]}
                         </div>
                         <div class="cara superior">
-                            <i class="bi bi-question-square" ></i>
+                            <i class="bi bi-question-square"></i>
                         </div>
                     </div>
                 </div>        
                 `)
-        //<img src="assent/images/wakame.jpeg" class="img-fluid d-block mx-auto my-auto"  alt="...">
-
-        // style="font-size:600%" style="height: 100px; width: 100px;"
-        if (i % 2 == 1) {
+        textos.push(descripciones[0])
+        headers.push(titulos[0])
+        if (i % 2 == 1) {  // por cada iteracion le quita el primer elemento a iconos, es decir, el iconos[0] es removido.
             iconos.splice(0, 1)
-        }
+            descripciones.splice(0, 1)
+            titulos.splice(0, 1)
+        }   // para que luego de agregar dos veces un elemento, este se quite de la lista y se continue con los demas
     }
-    tarjetas.sort(() => Math.random() - 0.5)
+    let random = Math.random() - 0.5;
+    textos.sort(() => random)
+    tarjetas.sort(() => random)
+    headers.sort(() => random)
+    descripciones = textos
+    titulos = headers
     tablero.innerHTML = tarjetas.join(" ")
 }
 
@@ -180,6 +258,18 @@ function deseleccionar(selecciones) {
             })
             const modalToggle = document.getElementById('modalMensaje');
             myModal.show(modalToggle);
+            modalToggle.querySelector(".modal-title").innerHTML = titulos[selecciones[0]]
+            modalToggle.querySelector(".modal-body").innerHTML = descripciones[selecciones[0]]
+
+            /*             if (selecciones[0] > 4) {
+                            if (selecciones[0] % 2 == 0) {
+                                modalToggle.querySelector(".modal-body").innerHTML = descripciones[selecciones[0]/2]
+                            } else {
+                                modalToggle.querySelector(".modal-body").innerHTML = descripciones[selecciones[1]/2]
+                            }
+                        } else {
+                            modalToggle.querySelector(".modal-body").innerHTML = descripciones[selecciones[0]]
+                        } */
             //document.getElementById('modalMensaje').modal({ show:true })
         }
         // const modalMensaje = document.getElementById('modalMensaje')
